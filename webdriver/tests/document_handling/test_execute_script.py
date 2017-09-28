a0726304f68d2bfaeaffa76ca1f1c6fca4b8becc
Reinhold Degenfellner <reinhold.jun@degenfellner.com>
2017-09-28 10:51:04 +0200

    parameterize tests to be able to use the same for execute_script and execute_async_script

--- webdriver/tests/document_handling/test_execute_script.py
+++ webdriver/tests/document_handling/test_execute_script.py
@@ -3,7 +3,7 @@ import pytest
 from tests.support.asserts import assert_same_element
 from tests.support.inline import inline
 from webdriver.client import element_key
-
+import execute_util
 
 # Ported from Mozilla's marionette's test suite.
 
@@ -25,78 +25,100 @@ def assert_elements_equal(session, expected, actual):
 # can be executed. It's unknown whether the default start
 # page for all browsers allows JS execution.
 
+def asyncExecute (session, script, args = []):
+    newScript = script.replace("arguments[", "actualArgs[")
+    val = session.execute_async_script("""
+        var nrActualArgs = arguments.length - 1;
+        var callback = arguments[nrActualArgs];
+        var actualArgs = []; 
+        for(var i = 0; i < nrActualArgs; ++i) {
+            actualArgs.push(arguments[i]); 
+        }
+        callback(function() { """ + newScript + ";}());", args)
+    return val
 
-def test_return_number(session):
+def syncExecute (session, script, args = []):
+    return session.execute_script(script, args)
+
+executors = [
+    syncExecute,
+    asyncExecute
+]
+
+
+@pytest.mark.parametrize("executor", executors)
+def test_return_number(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert 1 == session.execute_script("return 1")
-    assert 1.5 == session.execute_script("return 1.5")
+    assert 1 == executor(session,"return 1")
+    assert 1.5 == executor(session, "return 1.5")
 
-
-def test_return_boolean(session):
+@pytest.mark.parametrize("executor", executors)
+def test_return_boolean(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert True == session.execute_script("return true")
+    assert True == executor(session, "return true")
 
-
-def test_return_string(session):
+@pytest.mark.parametrize("executor", executors)
+def test_return_string(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert "foo" == session.execute_script("return 'foo'")
+    assert "foo" == executor(session, "return 'foo'")
 
-
-def test_return_array(session):
+@pytest.mark.parametrize("executor", executors)
+def test_return_array(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert [1, 2] ==  session.execute_script("return [1, 2]")
-    assert [1.25, 1.75] ==  session.execute_script("return [1.25, 1.75]")
-    assert [True, False] == session.execute_script("return [true, false]")
-    assert ["foo", "bar"] == session.execute_script("return ['foo', 'bar']")
-    assert [1, 1.5, True, "foo"] == session.execute_script("return [1, 1.5, true, 'foo']")
-    assert [1, [2]] == session.execute_script("return [1, [2]]")
+    assert [1, 2] ==  executor(session, "return [1, 2]")
+    assert [1.25, 1.75] ==  executor(session, "return [1.25, 1.75]")
+    assert [True, False] == executor(session, "return [true, false]")
+    assert ["foo", "bar"] == executor(session, "return ['foo', 'bar']")
+    assert [1, 1.5, True, "foo"] == executor(session, "return [1, 1.5, true, 'foo']")
+    assert [1, [2]] == executor(session, "return [1, [2]]")
 
-
-def test_return_object(session):
+@pytest.mark.parametrize("executor", executors)
+def test_return_object(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert {"foo": 1} == session.execute_script("return {foo: 1}")
-    assert {"foo": 1.5} == session.execute_script("return {foo: 1.5}")
-    assert {"foo": True} == session.execute_script("return {foo: true}")
-    assert {"foo": "bar"} == session.execute_script("return {foo: 'bar'}")
-    assert {"foo": [1, 2]} == session.execute_script("return {foo: [1, 2]}")
+    assert {"foo": 1} == executor(session, "return {foo: 1}")
+    assert {"foo": 1.5} == executor(session, "return {foo: 1.5}")
+    assert {"foo": True} == executor(session, "return {foo: true}")
+    assert {"foo": "bar"} == executor(session, "return {foo: 'bar'}")
+    assert {"foo": [1, 2]} == executor(session, "return {foo: [1, 2]}")
     assert {"foo": {"bar": [1, 2]}} == \
-        session.execute_script("return {foo: {bar: [1, 2]}}")
+        executor(session, "return {foo: {bar: [1, 2]}}")
 
-
-def test_no_return_value(session):
+@pytest.mark.parametrize("executor", executors)
+def test_no_return_value(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert None == session.execute_script("true")
+    assert None == executor(session, "true")
 
-
-def test_argument_null(session):
+@pytest.mark.parametrize("executor", executors)
+def test_argument_null(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert None == session.execute_script("return arguments[0]", args=[None])
+    assert None == executor(session, "return arguments[0]", args=[None])
 
-
-def test_argument_number(session):
+@pytest.mark.parametrize("executor", executors)
+def test_argument_number(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert 1 == session.execute_script("return arguments[0]", args=[1])
-    assert 1.5 == session.execute_script("return arguments[0]", args=[1.5])
+    assert 1 == executor(session, "return arguments[0]", args=[1])
+    assert 1.5 == executor(session, "return arguments[0]", args=[1.5])
 
-
-def test_argument_boolean(session):
+@pytest.mark.parametrize("executor", executors)
+def test_argument_boolean(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert True == session.execute_script("return arguments[0]", args=[True])
+    assert True == executor(session, "return arguments[0]", args=[True])
 
-
-def test_argument_string(session):
+@pytest.mark.parametrize("executor", executors)
+def test_argument_string(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert "foo" == session.execute_script("return arguments[0]", args=["foo"])
+    assert "foo" == executor(session, "return arguments[0]", args=["foo"])
 
-
-def test_argument_array(session):
+@pytest.mark.parametrize("executor", executors)
+def test_argument_array(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert [1, 2] == session.execute_script("return arguments[0]", args=[[1, 2]])
+    assert [1, 2] == executor(session, "return arguments[0]", args=[[1, 2]])
 
 
-def test_argument_object(session):
+@pytest.mark.parametrize("executor", executors)
+def test_argument_object(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert {"foo": 1} == session.execute_script(
+    assert {"foo": 1} == executor(session, 
             "return arguments[0]", args=[{"foo": 1}])
 
 globals = set([
@@ -104,54 +126,60 @@ globals = set([
               "navigator",
               "window",
               ])
-
-def test_access_todefault_globals(session):
+@pytest.mark.parametrize("executor", executors)
+def test_access_todefault_globals(session, executor):
     session.url = inline("""<title>JS testing</title>""")
     for property in globals:
-        assert property == session.execute_script(
+        assert property == executor(session, 
             "return typeof arguments[0] != 'undefined' ? arguments[0] : null",
             args=[property])
 
 
-def test_return_web_element(session):
+@pytest.mark.parametrize("executor", executors)
+def test_return_web_element(session, executor):
     session.url = inline("""<body><p>Cheese""")
     expected = session.find.css("p", all=False)
-    actual = session.execute_script(
+    actual = executor(session, 
             "return document.querySelector('p')")
     assert_same_element(session, expected.json(), actual)
 
 
-def test_return_web_element_array(session):
+@pytest.mark.parametrize("executor", executors)
+def test_return_web_element_array(session, executor):
     session.url = inline("""<p>Cheese <p>Peas""")
     expected = session.find.css("p")
-    actual = session.execute_script("""
+    actual = executor(session, """
             let els = document.querySelectorAll('p');
             return [els[0], els[1]]""")
     assert_elements_equal(session, expected, actual)
 
 
-def test_return_web_element_nodelist(session):
+@pytest.mark.parametrize("executor", executors)
+def test_return_web_element_nodelist(session, executor):
     session.url = inline("""<p>Peas <p>Cheese""")
     expected = session.find.css("p")
-    actual = session.execute_script("return document.querySelectorAll('p')")
+    actual = executor(session, "return document.querySelectorAll('p')")
     assert_elements_equal(session, expected, actual)
 
 
-def test_sandbox_reuse(session):
+@pytest.mark.parametrize("executor", executors)
+def test_sandbox_reuse(session, executor):
     session.url= inline("""<title>JS testing</title>""")
-    session.execute_script("window.foobar = [23, 42];")
-    assert [23, 42] == session.execute_script("return window.foobar")
+    executor(session, "window.foobar = [23, 42];")
+    assert [23, 42] == executor(session, "return window.foobar")
 
 
-def test_no_args(session):
+@pytest.mark.parametrize("executor", executors)
+def test_no_args(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    assert session.execute_script(
+    assert executor(session, 
         "return typeof arguments[0] == 'undefined'")
 
 
-def test_toJSON(session):
+@pytest.mark.parametrize("executor", executors)
+def test_toJSON(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    foo = session.execute_script("""
+    foo = executor(session, """
             return {
               toJSON () {
                 return "foo";
@@ -160,29 +188,32 @@ def test_toJSON(session):
     assert "foo" == foo
 
 
-def test_unsafe_toJSON(session):
+@pytest.mark.parametrize("executor", executors)
+def test_unsafe_toJSON(session, executor):
     session.url = inline("""<title>JS testing</title>""")
-    el = session.execute_script("""
+    el = executor(session, """
             return {
               toJSON () {
                 return document.documentElement;
               }
             }""")
-    expected = session.execute_script("return document.documentElement")
+    expected = executor(session, "return document.documentElement")
     assert_same_element(session, expected, el)
 
 
-def test_html_all_collection(session):
+@pytest.mark.parametrize("executor", executors)
+def test_html_all_collection(session, executor):
     session.url = inline("<p>foo <p>bar")
-    els = session.execute_script("return document.all")
+    els = executor(session, "return document.all")
 
     # <html>, <head>, <body>, <p>, <p>
     assert 5 == len(els)
 
 
-def test_html_form_controls_collection(session):
+@pytest.mark.parametrize("executor", executors)
+def test_html_form_controls_collection(session, executor):
     session.url = inline("<form><input><input></form>")
-    actual = session.execute_script("return document.forms[0].elements")
+    actual = executor(session, "return document.forms[0].elements")
     expected = session.find.css("input")
     assert_elements_equal(session, expected, actual)
 
